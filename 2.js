46144dd59658e94f4245f75dbb713318e1da9bc9
John Ley <ley.johnt@gmail.com>
2018-12-01 23:12:09 -0800

    day 1, day2

---
+++ 2.js
@@ -0,0 +1,57 @@
+const data = ['wlpiogsvdfecjdqmnxakudrhbz', 'wbpioesvyfecjuqmlxaktdrhbz', 'blviogavyfecjuqmnxaktdrhbz', 'wlpiogsvydecjuqmnipktdrhbz', 'wlwiogsvyfmcjuqmoxaktdrhbz', 'wlpiogsvphecjuqmnxaktdrzbz', 'wlpiogsvyfecjuqmnkakhdrkbz', 'wlpiogsvyfhcjuqmnxxktddhbz', 'wlpiogsvyfccfuqmnxgktdrhbz', 'wlpiogsvhmecjvqmnxaktdrhbz', 'wlpiogsvyfecjdqqnxaktdrhyz', 'wlpiogyvycecjaqmnxaktdrhbz', 'wlpiogsvyfecjfqmnxaktybhbz', 'wkpiogsvyfmcauqmnxaktdrhbz', 'wlmiolsvyfecjuqmnxaktdrhbn', 'wlpioksvyfecjuqmnxaktdrhgs', 'wlpiogsvyflcjuvmnxsktdrhbz', 'wgziogsvyfecjuqmnxaktdrhoz', 'whpingsvyeecjuqmnxaktdrhbz', 'wlpiogsvyfecjuqgnxaktdvhlz', 'wlpioasvtfecjuqmnxaktdahbz', 'wlpihgsvefeceuqmnxaktdrhbz', 'wlpiogsvyfecyuqwnxaktdghbz', 'wlpfsgsvyfhcjuqmnxaktdrhbz', 'wlpiogyvyfecjuqmnxalpdrhbz', 'wlpiogsvyfescsqmnxaktdrhbz', 'wluiogsvyfecytqmnxaktdrhbz', 'wltiorsvyfecjuqmoxaktdrhbz', 'wlmiogwvyfejjuqmnxaktdrhbz', 'wlpiogsvyfycjuumnxvktdrhbz', 'wlkiogsqyfecjqqmnxaktdrhbz', 'wlpiogsvyfecouqmnxaktfrubz', 'hupiogsvlfecjuqmnxaktdrhbz', 'wlpiogsvpxecjuqmnxaksdrhbz', 'wlpiogsvyfkcjfqmnxxktdrhbz', 'wlpiogsjyfecjuqnnxakthrhbz', 'wlpiogsvyfecjuqmnxaktddhdk', 'wlpipgsvyfecjuqmnhaktdrubz', 'wlpiogsoyfenjpqmnxaktdrhbz', 'wlpiogsvyfecnuqmnxaxtdrmbz', 'wlpiggsvyfjcjuumnxaktdrhbz', 'wlppogsvyfecjuqmnxautdhhbz', 'wlpiovbvyfecjuqmnxaktcrhbz', 'wlpiogsvyfecjoqmnxaktdrobu', 'wlpiohsvyfecjugmnxakthrhbz', 'wvpiogovyfecjuqmnxakwdrhbz', 'wlpiogsbyfecjuqmnxdktdrtbz', 'wlpnogsvyfecjuqmnxakykrhbz', 'wlpiogpvyfecjuqmnxvktdrhbs', 'wlpiogsvkvecjuqmnxadtdrhbz', 'wlkihgsvyfecjuqmnxlktdrhbz', 'wlpilgsvyfhcjuqmnxakudrhbz', 'wlpioksvysgcjuqmnxaktdrhbz', 'wlpiogsvyfecorqmoxaktdrhbz', 'wlpiogsvyfectzlmnxaktdrhbz', 'wlpiogsvywecjuqwnqaktdrhbz', 'wlpiowsvyfecjuqrnxaftdrhbz', 'wlpiogsuyfecjutmnxaktnrhbz', 'wepiowsvyfqcjuqmnxaktdrhbz', 'wlpirgssyfecjuqmvxaktdrhbz', 'wlyiogstyfecjuqmnxaktdrhbw', 'wlpiogseyfecauqmnxaktdjhbz', 'wlpioisvyfenjuqmnxakrdrhbz', 'wlpiogsvyfrgjfqmnxaktdrhbz', 'wlpionsvyfecjmqmjxaktdrhbz', 'alpiggsvyfecjuqmnxaktdrhkz', 'wlphngsvyfecjuqmnxfktdrhbz', 'wlpiogsvyferjuqmlxakttrhbz', 'wlniogsvefecjuqsnxaktdrhbz', 'wlpiogsvyfncjurmnxakudrhbz', 'wlpiogsvyfecjuqmnxaktprlaz', 'wlpiocsvyfecjupmkxaktdrhbz', 'wlpihgsvyfecjeqfnxaktdrhbz', 'wlwioasvyfjcjuqmnxaktdrhbz', 'wlpifgsvyfecjuqsnxaktdshbz', 'wlxiogsvyrechuqmnxaktdrhbz', 'wlpiogovyfxcjuqmnxakkdrhbz', 'wlpiogsvyfecjkqmdxaktmrhbz', 'wlpiogsvyfecjwqmntaktdhhbz', 'wlpiogsvdfecjuqmmxaktbrhbz', 'wlpiogsvyfecauqmnxaksdrhwz', 'wlpiogsvwfecjuqznxaktorhbz', 'wlpiogtvyfecjuqhnxakidrhbz', 'wlpiogsvyyecjuqmnxaktdrhwt', 'wljiogsvyfecfuqbnxaktdrhbz', 'wlpiogsvybecjuqmnxagtdrjbz', 'wrpiogsvyfecjuqmnuaktdrhbd', 'wlpiogsvyfecjurmnxnltdrhbz', 'blpvogsvyaecjuqmnxaktdrhbz', 'bfpiogyvyfecjuqmnxaktdrhbz', 'wlpiogsvyfecjuqinxaknddhbz', 'wlpizgsvvfecjuqxnxaktdrhbz', 'glpiogsvyrecjuqmnxaktdrhbr', 'wlpiogskhfecjutmnxaktdrhbz', 'wlpiogsvyfecmuqmnxaktdribc', 'wlpioesvwfecjuqmnxakkdrhbz', 'wlpionsrafecjuqmnxaktdrhbz', 'wlsiogsvyfecjuqmnaaktdrhvz', 'bloiogsvyfecjuqmnxakjdrhbz', 'wlpiogsvyfecjuqenmastdrhbz', 'wlpiogyvyfecjuqmuxakldrhbz', 'plpiogovyfecjuvmnxaktdrhbz', 'wlpiogsvyfebjuqmnkakvdrhbz', 'wlziogsvyfhcjuqmngaktdrhbz', 'wlsiogsvyfecjuqmnxaktdrzjz', 'plbiogsvyfecfuqmnxaktdrhbz', 'wfpiogsvyfecjuqknxaktdrhiz', 'wlpiogjbyfecjuqmnxaktprhbz', 'wmpiogsvyrecjcqmnxaktdrhbz', 'wlpiogsyyfecjuqmqxaktdrbbz', 'wlpiogsvyfecjuqknxaetdehbz', 'wlpiogsvyfezjuqmnxakkdhhbz', 'wlpiogsvyfecjjqvnxaktdrhiz', 'wkpiogsvyfucjuqmnxaktdrhbd', 'lliiogsvyfecjuqmnxaktdrhoz', 'wlpiogsvyfecjuqmsxdktdshbz', 'wlprogtvyfecjuqmnxaktvrhbz', 'wlpizgssyffcjuqmnxaktdrhbz', 'wlpioasvyfvcjuqmnxakldrhbz', 'wlpoogsvyyecjuqmnxastdrhbz', 'wlpiognvyfecjuqmnsaktdrhbr', 'wlpiogsoyfecjuqmnxaktdrhho', 'wfpiogsvydecjuqmnxaotdrhbz', 'wlpiogsvqhecjuqmnxaktdrhhz', 'wkpiogsvyfeojuqmnxaktdrqbz', 'wlpiogsvyfeveuqmnxaktdshbz', 'wlpiogbvyfecjuqmexaktdrcbz', 'wlpxogsvyfehjsqmnxaktdrhbz', 'wlpcogsvyfecjuqmnjakttrhbz', 'wlpiogsvvkecjuqmnxaftdrhbz', 'wlpiogsvffecnuqmnxaktdnhbz', 'wlpiogsvyfecjupjnxaktdrhbr', 'wlpqogsvyfecjuqmnxlktdphbz', 'wlpxogsvyfecjvqmnxaktirhbz', 'elpiogsvyfecjuqlnxaqtdrhbz', 'wspiogsvrfecjuqmnxakadrhbz', 'wlpiogsmyfecbuqmnxactdrhbz', 'wlpiogsvyfecauqmnyakzdrhbz', 'wlsiogsvyfecjuqmnxakvdrnbz', 'wlpiogsxyfeijuqmnxakndrhbz', 'wlpiogsvyfecjuumnxakbyrhbz', 'wlpiogsvyfecjuqmnxhktdyhbo', 'wlpiogsvyfecjuqqnxnjtdrhbz', 'wapiogsvyfecjuqmnxoktdrmbz', 'wlpiogsvyfeejvqmnxaktdrubz', 'wlpitgsvyeectuqmnxaktdrhbz', 'alpiogsvyfecjulmnxaktdchbz', 'wlpiogsvyfecjuqmuxoktdrwbz', 'wlpiogsvyfzgjuhmnxaktdrhbz', 'wlpnogsvyfecjuqmdxaktyrhbz', 'wlpiogsvyfecjuqmnxakthrhra', 'wliiogsvyfecluqmnxaktdhhbz', 'wlpiogsvyfecjuymnxaltdrhwz', 'wlpiogsvyfeljuqmnxaktyrhbd', 'wlpiygsvvfecjuqmfxaktdrhbz', 'wlpiogihsfecjuqmnxaktdrhbz', 'wlpiogjvyfecjuqmnhuktdrhbz', 'wldiogsvyfecjiqmwxaktdrhbz', 'wlpiogsvjfecjuqmnxaktdrgbr', 'wlpioisvyfecjuqwnxabtdrhbz', 'wlviogsvyfscjuqmnxqktdrhbz', 'wlpiogsvyfecjuqmuxakbdrubz', 'wlpiogsvyfecjuqmnxmatdrhqz', 'wlpiogsvyfbcjuqwmxaktdrhbz', 'wlpiogsvyfexjuqmnxuxtdrhbz', 'wljiogsvbfecjuqmnxartdrhbz', 'wlpvogsvyfeujuqmnxaktdmhbz', 'wnpiogsvyfekjuqanxaktdrhbz', 'wlprogsvyfecjuqmzxvktdrhbz', 'wkpiogvvyfecjuqmnxaktdrabz', 'wlpiogsvwfecjuqmnxaktkbhbz', 'wlpiogsvyfecjlqmnxtttdrhbz', 'wlpioqsvyfecjuqznxaktyrhbz', 'wlpiogsvyfecjuqmnxnethrhbz', 'wlpiogsyyfgcjuqmnxaktdrhbm', 'wlpiopsvbfecjuqmnxaktdlhbz', 'wloqogsvyfucjuqmnxaktdrhbz', 'wlpiogsvmfecjuqmnxmktdrhtz', 'wlhiogsvyfecjuhmnxaktsrhbz', 'wlpioggvpfecjufmnxaktdrhbz', 'wlpiogsvyfbcjuomnxaktdrhbh', 'wlpmogsvyfecyuqmnxoktdrhbz', 'wlpiogslyfecjuqmnxaptyrhbz', 'tlpiogsnyfecguqmnxaktdrhbz', 'wlpiogsvyfecjuqmnxwktwehbz', 'wlpiogsvgfecjuqmnxaktdrabu', 'wbpiogsvyfecjuqmnxagtdrhoz', 'wlwipgsvyfecjuqmnxaktdrhbu', 'wlpwogsvykeczuqmnxaktdrhbz', 'wlpgogsvwfecjuqmnxcktdrhbz', 'wlpiogsqyfecjuqmrxaktdrrbz', 'wlpiogsvyfecjuqmnxakthrafz', 'wypicgseyfecjuqmnxaktdrhbz', 'wlpiogcvqfecjuqmnxaktdrhzz', 'wlriogsvyfecouqmnkaktdrhbz', 'wlpiogsvyfemjulmnxaktdrhdz', 'flpiogadyfecjuqmnxaktdrhbz', 'wupiogsvyfbvjuqmnxaktdrhbz', 'wlpiogsvyfebjummnxaktdrrbz', 'wjpiogsvyfecjuqmnxaktprybz', 'wlpirgsvyfecjiqmnxaatdrhbz', 'bvpiogsvyfecjuqmnxaktdrhez', 'wlpiogsvyfxcjuqmnxykzdrhbz', 'wlkiwgsqyfecjqqmnxaktdrhbz', 'wepaogsvyfecjxqmnxaktdrhbz', 'wlpiovsvyfecjjqmnxaktdmhbz', 'wlpioysryfecjuqmnxaktdrhiz', 'wlpizjsvyfecjuvmnxaktdrhbz', 'dlpiogsvyfecjucmnxakbdrhbz', 'wlpiogsccfecjrqmnxaktdrhbz', 'wlpioggvyfecpuqmnxagtdrhbz', 'wlpiogsvyfvcjuumlxaktdrhbz', 'wwpiogsryfjcjuqmnxaktdrhbz', 'wlpiogsvyfecjuqynxaktdrogz', 'wlpiogsvyfecjpqmnxskbdrhbz', 'wlpiogsvyfecjuhmfxaktvrhbz', 'wlpiogevyfecjrqmnwaktdrhbz', 'wlpiigsvyfemjuqmnxaktdrhtz', 'wlpcogsvyfecjuqhnxakgdrhbz', 'wupiogsvyfxcjuqmnxaktdrhgz', 'wlsiogsvyfecjuqenxuktdrhbz', 'wlpioglvyfecjujmexaktdrhbz', 'wlriogsvyfeljuqmnxattdrhbz', 'wlpiogsvyfecfuqmhxaktkrhbz', 'wlppogsvyfecjuqmxxabtdrhbz', 'wlniogsvyfevjuqwnxaktdrhbz', 'wlhiogsvyfecjuqmnxactxrhbz', 'ilpiogivyflcjuqmnxaktdrhbz', 'wlpmogsvyfecjuqmnxaktdrlbs', 'wipiogsvyfeqjuqmnxaktrrhbz', 'wvpiogsvyfecjuqknxaktdrrbz', 'wwpioguvyfecxuqmnxaktdrhbz', 'wlpiogsvkfdcjuqmnxaktdzhbz', 'wlpiogfvyfecjuqmnxadtdrhbg', 'wlpiogsvyzefjuqfnxaktdrhbz', 'wlpiogstyfechuqmnxaktdchbz', 'wlpiogszyfedjuqmnxsktdrhbz', 'wzpiozsvyfncjuqmnxaktdrhbz', 'xlpiogsvyfefjuqmnmaktdrhbz', 'wlpiogsvyfebxummnxaktdrhbz', 'wlpiogsgyfecfurmnxaktdrhbz', 'wlpqogsvyfecjuomnxaktdrhbi', 'wlpiogjvufecjuqmnxaktdrhbd', 'wlpiolsvyfecduqmnxaktrrhbz', 'wlpxogsvyfecjuqmnxaktgrhbk', 'wlpiogsfyfncjuqmnxsktdrhbz', 'wlpioggvyfecjufmnxaktdrebz', 'wlpiogsvyfecfujmnxaktdrwbz', 'rlpiogsvyfecjlqmnxaktdqhbz', 'wlpfogsvyfecjuimnxaktfrhbz']
+
+/* PART ONE */
+const checksum = data => {
+    let doubles = 0
+    let triples = 0
+
+    data.forEach(id => {
+        const charFrequency = Object.values(
+            id.split('').reduce((dict, char) => {
+                if (dict.hasOwnProperty(char)) { dict[char] += 1 }
+                else { dict[char] = 1 }
+
+                return dict
+            }, {})
+        )
+
+        let double = false
+        let triple = false
+
+        for (let i = 0; i < charFrequency.length; i++) {
+            const freq = charFrequency[i]
+
+            if (!double && freq === 2) { double = true; doubles++ }
+            if (!triple && freq === 3) { triple = true; triples++ }
+
+            if (double && triple) break
+        }
+    })
+
+  return doubles * triples
+}
+
+/* PART TWO */
+let lastDiffIndex = null
+
+const diffByOne = (a, b) => {
+    let diffAtSameIndex = 0
+
+    for (let i = 0; i < a.length; i++) {
+  	    if (a[i] !== b[i]) { diffAtSameIndex++; lastDiffIndex = i }
+        if (diffAtSameIndex > 1) return false
+    }
+
+    return true
+}
+
+const findDiffByOneCommonChars = (data, i = 0) => {
+    const curr = data[i]
+    const match = data.slice(++i).find(candidate => diffByOne(curr, candidate))
+
+    return match === undefined ? findDiffByOneCommonChars(data, i) : match.slice(0, lastDiffIndex) + match.slice(++lastDiffIndex)
+}
+
+console.log(checksum(data)) // part 1
+console.log(findDiffByOneCommonChars(data)) // part 2
+
